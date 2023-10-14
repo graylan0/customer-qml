747359cf07bf5a909cd4c564e83366082c1e4242
Graylan <34530588+graylan0@users.noreply.github.com>
2023-10-14 09:04:27 -0400

    Update app.py
    LOVE YOUR ENEMIES

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import eel
 import json
 import asyncio
+import re
 import logging
 from concurrent.futures import ThreadPoolExecutor
 import numpy as np
@@ -29,7 +30,33 @@ llm = Llama(
 
 executor = ThreadPoolExecutor(max_workers=3)
 
-# Function to run Llama model
+# Function to query Weaviate for phone recommendations based on criteria
+async def query_weaviate_for_phones(keywords):
+    response = (
+        client.query
+        .get("Phone", ["name", "description", "price"])
+        .with_near_text({"concepts": keywords})
+        .with_limit(2)
+        .do()
+    )
+    return response['data']
+
+# Function to extract keywords using Llama2
+async def extract_keywords_with_llm(prompt):
+    keyword_extraction_prompt = f"Extract important keywords from the following customer query to search in Weaviate: '{prompt}'"
+    response = llm(keyword_extraction_prompt, max_tokens=100)['choices'][0]['text']
+    
+    # Use regular expressions to extract keywords
+    keywords = re.findall(r'\b\w+\b', response)
+    
+    # If the reply doesn't contain satisfactory keywords, regenerate
+    if not keywords or len(keywords) < 2:
+        response = llm(keyword_extraction_prompt, max_tokens=100)['choices'][0]['text']
+        keywords = re.findall(r'\b\w+\b', response)
+    
+    return keywords
+
+# Function to run Llama model and query Weaviate for phone recommendations
 async def run_llm(prompt):
     agi_prompt = ("You are a Verizon Service Sales Representative AI Ambassador. "
                   "Your job is critical. You are responsible for helping customers, "
@@ -40,10 +67,27 @@ async def run_llm(prompt):
                   "3. Ensuring customer satisfaction.\n"
                   "4. Offering interactive engagement to keep customers entertained while they wait.\n")
     
-    query_prompt = f"A customer is looking for a phone and said: '{prompt}'. What are the best 2 phones you recommend and why?"
+    # Extract keywords dynamically from the user's prompt using Llama2
+    keywords = await extract_keywords_with_llm(prompt)
+    
+    # Query Weaviate for phone recommendations based on the extracted keywords
+    recommended_phones = await query_weaviate_for_phones(keywords)
+    
+    # Prepare the phone recommendations for inclusion in the Llama model prompt
+    phone_recommendations = ""
+    if recommended_phones:
+        for phone in recommended_phones:
+            phone_recommendations += f"\n- {phone['name']}: {phone['description']} (Price: {phone['price']})"
+    
+    query_prompt = f"A customer is looking for a phone and said: '{prompt}'. What are the best 2 phones you recommend and why? Here are some options based on your criteria:{phone_recommendations}"
+    
     full_prompt = agi_prompt + query_prompt
+    
+    # Generate the response using the Llama model
     response = llm(full_prompt, max_tokens=900)['choices'][0]['text']
+    
     return response
+
 
 # BarkTTS function
 def generate_audio_for_sentence(sentence):
@@ -83,3 +127,4 @@ if __name__ == "__main__":
     nest_asyncio.apply()
     asyncio.run(main())
     eel.start('index.html')
+
